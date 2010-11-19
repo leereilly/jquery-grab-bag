0379bfa2d801bb1adddebfc63ba859d3e6806dcc
Lee Reilly <lee@leereilly.net>
2010-11-18 20:42:42 -0800

    First bash at DNA/binary decryption

--- javascripts/jquery.text-effects.js
+++ javascripts/jquery.text-effects.js
@@ -1,4 +1,44 @@
 (function($) {
+
+    function randomDNA() {
+        var rnd = Math.floor(Math.random() * 3);
+	if (rnd == 0) return 'A'
+        else if (rnd == 1) return 'C'
+        else if (rnd == 2) return 'T'
+        else return 'G'
+    }
+
+    function randomBinary() {
+        return Math.floor(Math.random() * 2);
+    }
+    
+    $.fn.dnaDecrypt = function() {
+        this.each(function() {
+            var $ele = $(this), str = $ele.text(), progress = 0, replace = /[^\s]/g,
+                random = randomDNA, inc = 3;
+            $ele.text('');
+            var timer = setInterval(function() {
+                $ele.text(str.substring(0, progress) + str.substring(progress, str.length).replace(replace, random));
+                progress += inc
+                if (progress >= str.length + inc) clearInterval(timer);
+            }, 100);
+        });
+        return this;
+    };    
+
+    $.fn.binaryDecrypt = function() {
+        this.each(function() {
+            var $ele = $(this), str = $ele.text(), progress = 0, replace = /[^\s]/g,
+                random = randomBinary, inc = 3;
+            $ele.text('');
+            var timer = setInterval(function() {
+                $ele.text(str.substring(0, progress) + str.substring(progress, str.length).replace(replace, random));
+                progress += inc
+                if (progress >= str.length + inc) clearInterval(timer);
+            }, 100);
+        });
+        return this;
+    };    
     
     function shuffle(a) {
         var i = a.length, j;
@@ -87,4 +127,5 @@
         return this;
     };
     
+
 })(jQuery);
